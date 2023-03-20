eba04fb45f23ed0aadaefe99a57e296065604c47
Popil Roman <popil.roman23@mail.ru>
2023-03-21 00:50:19 +0500

    Создал классы scrapperpplication и botpplication с методом main в модулях scrapper и bot в пакетах app

---
+++ TinkoffProject/bot/src/main/java/app/BotApplication.java
@@ -0,0 +1,17 @@
+package app;
+
+import configuration.*;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(ApplicationConfig.class)
+public class BotApplication {
+    public static void main(String[] args) {
+        var ctx = SpringApplication.run(BotApplication.class, args);
+        ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
+        System.out.println(config);
+    }
+}
