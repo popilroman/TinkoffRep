149fa89b995e5aa19ca89f49302563699ba68f4b
Popil Roman <popil.roman23@mail.ru>
2023-03-21 20:23:43 +0500

    Реализовал парсер ссылок github и stackoverflow

--- TinkoffProject/link-parser/src/main/java/com/tinkoff/project/App.java
+++ TinkoffProject/link-parser/src/main/java/com/tinkoff/project/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String gitHubResult = LinkParser.parseLink("https://github.com/sanyarnd/tinkoff-java-course-2022/");
+        System.out.println(gitHubResult);
+        String stackOverflowResult = LinkParser.parseLink("https://stackoverflow.com/questions/1642028/what-is-the-operator-in-c");
+        System.out.println(stackOverflowResult);
+        String incorrectResult = LinkParser.parseLink("https://stackoverflow.com/search?q=unsupported%20link");
+        System.out.println(incorrectResult);
     }
 }
