bb9357131039729127959f60504cc09ae3780f52
Popil Roman <popil.roman23@mail.ru>
2023-04-03 09:46:26 +0500

    cherry-pick: Исправил ошибку связки свойсва app.scheduler.interval с объектом ApplicationConfig

--- TinkoffProject/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/ApplicationConfig.java
+++ TinkoffProject/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/ApplicationConfig.java
@@ -3,16 +3,32 @@ package ru.tinkoff.edu.java.scrapper.configuration;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
 import org.springframework.validation.annotation.Validated;
 import reactor.core.scheduler.Scheduler;
 
 import java.time.Duration;
 
-@Validated
-@ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record ApplicationConfig(@NotNull String test, Scheduler scheduler) {
-    @Value("${app.scheduler.interval}")
-    private static Duration schedulerInterval;
+@ConfigurationProperties(prefix = "app")
+public class ApplicationConfig {
+    private String test;
+    private Duration schedulerInterval;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public Duration getSchedulerInterval() {
+        return schedulerInterval;
+    }
+
+    public void setSchedulerInterval(Duration schedulerInterval) {
+        this.schedulerInterval = schedulerInterval;
+    }
 }
 
 
