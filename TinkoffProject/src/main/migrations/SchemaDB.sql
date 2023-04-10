e14224d2cd9b6ff237aec42c4d1f10dd2931235a
Popil Roman <popil.roman23@mail.ru>
2023-04-10 13:06:30 +0500

    Фиксы compose и SchemaDB.sql

--- TinkoffProject/src/main/migrations/SchemaDB.sql
+++ TinkoffProject/src/main/migrations/SchemaDB.sql
@@ -1,16 +1,18 @@
 --liquibase formatted sql
 
+SET search_path TO public;
+
 --changeset me:create-chat-table
-CREATE TABLE Чаты (
+CREATE TABLE public.Chats (
   id INT NOT NULL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL
+  name_chat VARCHAR(255) NOT NULL
 );
 
 --changeset me:create-link-table
-CREATE TABLE Ссылки (
+CREATE TABLE Links (
   id INT NOT NULL PRIMARY KEY,
   url VARCHAR(255) NOT NULL,
   date_added TIMESTAMP NOT NULL,
   id_chat INT NOT NULL,
-  CONSTRAINT fk_chat FOREIGN KEY (id_chat) REFERENCES Чаты(id)
+  CONSTRAINT fk_chat FOREIGN KEY (id_chat) REFERENCES public.Chats(id)
 );
