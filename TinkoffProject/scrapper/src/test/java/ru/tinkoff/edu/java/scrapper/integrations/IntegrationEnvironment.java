9d3d575d9146e41763af449026a93a88b1a3b211
Popil Roman <popil.roman23@mail.ru>
2023-04-15 17:54:22 +0500

    Добавил тесты

---
+++ TinkoffProject/scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/integrations/IntegrationEnvironment.java
@@ -0,0 +1,45 @@
+package ru.tinkoff.edu.java.scrapper.integrations;
+
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.DirectoryResourceAccessor;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class IntegrationEnvironment {
+    protected static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER;
+    private static final Path MIGRATIONS_PATH;
+
+    static {
+        POSTGRE_SQL_CONTAINER = new PostgreSQLContainer<>("postgres:15")
+                .withDatabaseName("scrapper")
+                .withUsername("postgres")
+                .withPassword("postgres");
+        POSTGRE_SQL_CONTAINER.start();
+
+        MIGRATIONS_PATH = new File("migrations").toPath().toAbsolutePath();
+
+        try (Connection connection = DriverManager.getConnection(POSTGRE_SQL_CONTAINER.getJdbcUrl(),
+                POSTGRE_SQL_CONTAINER.getUsername(), POSTGRE_SQL_CONTAINER.getPassword())){
+
+            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            Liquibase liquibase = new Liquibase("master.xml", new DirectoryResourceAccessor(MIGRATIONS_PATH), database);
+            liquibase.update(new Contexts(), new LabelExpression());
+
+        } catch (SQLException | FileNotFoundException | LiquibaseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
